e8c59c20e25f1bc828d7e30267316b558313b5de
takeitEasyhwan <wlghks0508@naver.com>
2023-05-29 18:19:40 +0900

    ver2

--- js/main.js
+++ js/main.js
@@ -894,7 +894,7 @@ $(document).ready(function () {
         }
       }
 
-      if(brickCnt > 0)
+      if(brickCnt>0)
         requestAnimationFrame(draw);
       else{
         $("#story").fadeIn();
